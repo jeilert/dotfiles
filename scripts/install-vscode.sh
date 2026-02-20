f42dc275f416ae3e8e588a0c852f0f3a9f6dd5af
jeilert <jorgen.eilertsen@proton.me>
2026-02-20 22:13:23 +0100

    changed vscode install to flatpak

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -3,20 +3,7 @@
 echo "=== Installing Visual Studio Code ==="
 echo ""
 
-# Add Microsoft repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-sudo cat > /etc/yum.repos.d/vscode.repo << EOF
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
-# Install
-sudo dnf install code -y
+flatpak install flathub com.visualstudio.code -y
 
 echo ""
 echo "Done! Visual Studio Code installed."
